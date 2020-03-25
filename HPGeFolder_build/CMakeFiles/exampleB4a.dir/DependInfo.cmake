2930cf9053a0a27c66884756d8388c33c6f7765d
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-03-25 11:31:52 +0000

    Add files via upload

---
+++ HPGeFolder_build/CMakeFiles/exampleB4a.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ishokar/GEANT4/HPGeFolder/exampleB4a.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4DetectorConstruction.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4DetectorConstruction.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4PrimaryGeneratorAction.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4RunAction.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4RunAction.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4aActionInitialization.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4aActionInitialization.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4aEventAction.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4aEventAction.cc.o"
+  "/home/ishokar/GEANT4/HPGeFolder/src/B4aSteppingAction.cc" "/home/ishokar/GEANT4/HPGeFolder_build/CMakeFiles/exampleB4a.dir/src/B4aSteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/unix/local/Geant4/Geant4-10.4.0-Linux/include/Geant4"
+  "/home/ishokar/GEANT4/HPGeFolder/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ HPGeFolder_build/CMakeFiles/exampleB4a.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4aEventAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4aActionInitialization.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4aSteppingAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4RunAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/B4DetectorConstruction.cc.o"
+  "exampleB4a.pdb"
+  "exampleB4a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/exampleB4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
