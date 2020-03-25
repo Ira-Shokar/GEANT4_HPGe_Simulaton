11af8543ea91b3f44742b5f91ae396ea6edcfdd4
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-03-25 11:30:28 +0000

    Add files via upload

---
+++ B4c_build/CMakeFiles/exampleB4c.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ishokar/GEANT4/B4c/exampleB4c.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/exampleB4c.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4PrimaryGeneratorAction.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4RunAction.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4RunAction.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4cActionInitialization.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4cActionInitialization.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4cCalorHit.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4cCalorHit.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4cCalorimeterSD.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4cCalorimeterSD.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4cDetectorConstruction.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4cDetectorConstruction.cc.o"
+  "/home/ishokar/GEANT4/B4c/src/B4cEventAction.cc" "/home/ishokar/GEANT4/B4c_build/CMakeFiles/exampleB4c.dir/src/B4cEventAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/unix/local/Geant4/Geant4-10.4.0-Linux/include/Geant4"
+  "/home/ishokar/GEANT4/B4c/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ B4c_build/CMakeFiles/exampleB4c.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/exampleB4c.dir/exampleB4c.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4cActionInitialization.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4cDetectorConstruction.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4cCalorHit.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4cCalorimeterSD.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4RunAction.cc.o"
+  "CMakeFiles/exampleB4c.dir/src/B4cEventAction.cc.o"
+  "exampleB4c.pdb"
+  "exampleB4c"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/exampleB4c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
